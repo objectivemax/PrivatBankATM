9e0eca5d162596db6d02a8aa87157d0de6e02336
objectivemax <objectivemax@gmail.com>
2017-10-12 22:24:41 +0300

    Initial Commit

---
+++ PrivateATM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrivateATM
+//
+//  Created by Maxim Ohrimenko on 10/12/17.
+//  Copyright © 2017 Maxim Ohrimenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PrivateATM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrivateATM
+//
+//  Created by Maxim Ohrimenko on 10/12/17.
+//  Copyright © 2017 Maxim Ohrimenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
